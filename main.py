b99a82a75638c16d22c112f7b3e77a73186e34cf
Fembunny <bellacyrne@gmail.com>
2025-05-15 20:26:39 -0300

    Update main()

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from games.flappy_birds.flappy import FlappyBirds
+from menu.button import Button
 
 def play_tetris ():
     print("Tetris")
@@ -37,25 +38,52 @@ def play_flappy() :
 
         pg.display.update()
 
+def get_font(size):
+    return pg.font.Font("assets/font.ttf", size)
+
 def main():
     # Inicialização do Pygame
     pg.init()
-    window_size = 42
-    screen = pg.display.set_mode((window_size * 14, window_size * 20))
 
+    screen = pg.display.set_mode((1280, 720))
     imagem = pg.image.load('assets/games.png')
     pg.display.set_icon(imagem)
     pg.display.set_caption("Mini Games")
+    background = pg.image.load("assets/tree.jpg")
 
     running = True
     while running:
-        for event in pg.event.get() :
+        screen.blit(background, (0, 0))
+
+        mouse_position = pg.mouse.get_pos()
+        menu_text = get_font(40).render("START GAME", True, "#b68f40")
+        menu_rect = menu_text.get_rect(center=(640, 280))
+
+        # Chamando a classe e criando os botões
+        tetris_button = Button(pos=(640, 350), text_input="Tetris", font=get_font(30), base_color="#d7fcd4", hovering_color="black")
+        flappy_button = Button(pos=(640, 410), text_input="Flappy-Birds", font=get_font(30), base_color="#d7fcd4", hovering_color="White")
+        quit_button = Button(pos=(640, 470), text_input="Quit", font=get_font(30), base_color="#d7fcd4", hovering_color="White")
+
+        screen.blit(menu_text, menu_rect)
+
+        # For para desenhar botões
+        for button in [tetris_button, flappy_button, quit_button]:
+            button.changeColor(mouse_position)
+            button.update(screen)
+
+        for event in pg.event.get():
             if event.type == pg.QUIT :
                 running = False
-            if event.type == pg.KEYDOWN:
-                # IF DE TESTE PARA VERIFICAR A TELA DO FLAPPY
-                if pg.key.name(event.key) == 'escape':
-                    return play_flappy()
+
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if tetris_button.checkForInput(mouse_position) :
+                    play_flappy()
+                if flappy_button.checkForInput(mouse_position) :
+                    play_flappy()
+                if quit_button.checkForInput(mouse_position) :
+                    running = False
+
+        pg.display.update()
 
     pg.quit()
 
